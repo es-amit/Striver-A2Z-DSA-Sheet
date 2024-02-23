9d6a11da95319598f669655feafc3ded854e006a
amit <amit.singh.9312687983@gmail.com>
2024-02-23 22:56:44 +0530

    Traversals

---
+++ Binary Trees/LeftView.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#include<TreeNode.cpp>
+using namespace std;
+
+void solve(Node* root,vector<int> &ans,int level){
+    if(root == nullptr) return;
+    
+    if(level == ans.size()){
+        ans.push_back(root->data);
+    }
+    solve(root->left,ans,level+1);
+    solve(root->right,ans,level+1);
+}
+vector<int> leftView(Node *root)
+{
+   vector<int> ans;
+   solve(root,ans,0);
+   return ans;
+}
---
+++ Binary Trees/TopView.cpp
@@ -0,0 +1,36 @@
+#include <TreeNode.cpp>
+using namespace std;
+vector<int> topView(Node *root)
+{
+    vector<int> ans;
+    if (root == nullptr)
+        return ans;
+    map<int, int> topNodes;
+    queue<pair<Node *, int>> q;
+    q.push(make_pair(root, 0));
+    while (!q.empty())
+    {
+        pair<Node *, int> temp = q.front();
+        q.pop();
+
+        Node *frontNode = temp.first;
+        int hd = temp.second;
+        if (topNodes.find(hd) == topNodes.end())
+        {
+            topNodes[hd] = frontNode->data;
+        }
+        if (frontNode->left)
+        {
+            q.push(make_pair(frontNode->left, hd - 1));
+        }
+        if (frontNode->right)
+        {
+            q.push(make_pair(frontNode->right, hd + 1));
+        }
+    }
+    for (auto i : topNodes)
+    {
+        ans.push_back(i.second);
+    }
+    return ans;
+}
