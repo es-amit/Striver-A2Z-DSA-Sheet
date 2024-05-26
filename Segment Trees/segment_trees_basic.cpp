306e760d3b25144c8b45bc4a8ad2a1fc46720e4c
amit <amit.singh.9312687983@gmail.com>
2024-05-26 15:58:15 +0530

    Segment-Tree-find-min-template

--- Segment Trees/segment_trees_basic.cpp
+++ Segment Trees/segment_trees_basic.cpp
@@ -1,55 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
-int a[100005];
-int seg[4 * 100005];
 
-void build(int ind, int low, int high)
+class SegmentTree
 {
-    if (low == high)
-    {
-        seg[ind] = a[low];
-        return;
-    }
-    int mid = (low + high) / 2;
-    build(2 * ind + 1, low, mid);
-    build(2 * ind + 2, mid + 1, high);
-    seg[ind] = max(seg[2 * ind + 1], seg[2 * ind + 2]);
-}
+    vector<int> seg;
 
-int query(int ind, int low, int high, int l, int r)
-{
-    if (low >= l && high <= r)
+public:
+    SegmentTree(int n)
     {
-        return seg[ind];
-    }
-    if (high < l || low > r)
-        return INT_MIN;
-    int mid = (low + high) / 2;
-    int left = query(2 * ind + 1, low, mid, l, r);
-    int right = query(2 * ind + 2, mid + 1, high, l, r);
-    return max(left, right);
-}
-int main()
-{
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> a[i];
+        seg.resize(4 * n + 1);
     }
 
-    build(0, 0, n - 1);
-    int q;
-    cin >> q;
-    while (q)
+    void build(int ind, int low, int high, int arr[])
     {
-        int l, r;
-        cin >> l >> r;
-        cout << query(0, 0, n - 1, l, r);
-        q--;
+        if (low == high)
+        {
+            seg[ind] = arr[low];
+            return;
+        }
+        int mid = (low + high) >> 1;
+        build(2 * ind + 1, low, mid, arr);
+        build(2 * ind + 2, mid + 1, high, arr);
+        seg[ind] = min(seg[2 * ind + 1], seg[2 * ind + 2]);
     }
-}
+
+    int query(int ind, int low, int high, int l, int r)
+    {
+        // out of range
+        if (high < l || r < low)
+        {
+            return INT_MAX;
+        }
+        // completely lie between range
+        if (low >= l && high <= r)
+        {
+            return seg[ind];
+        }
+        int mid = (low + high) >> 1;
+        int left = query(2 * ind + 1, low, mid, l, r);
+        int right = query(2 * ind + 2, mid + 1, high, l, r);
+        return min(left, right);
+    }
+
+    void update(int ind, int low, int high, int i, int val)
+    {
+        if (low == high)
+        {
+            seg[ind] = val;
+            return;
+        }
+        int mid = (low + high) >> 1;
+        if (i <= mid)
+        {
+            update(2 * ind + 1, low, mid, i, val);
+        }
+        else
+        {
+            update(2 * ind + 2, mid + 1, high, i, val);
+        }
+        seg[ind] = min(seg[2 * ind + 1], seg[2 * ind + 2]);
+    }
+};
